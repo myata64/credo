1a246f58a0a2537668b39c2926eef5801f268e51
myata64 <sergiolaconnelly@B450M-DS3H>
2023-04-29 10:42:50 +0300

    new commit

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import json
+# from utils import get_news, search_movie, get_weather_data
+from datetime import datetime
 
 app = Flask(__name__)
 
 
-@app.route('/home')
+@app.route('/')
 def home_page():  # put application's code here
 	return render_template('home.html')
 
 @app.route('/brends')
 def shoes_page():
-	return render_template('brends.html')
+	return render_template('shoes.html')
 
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
